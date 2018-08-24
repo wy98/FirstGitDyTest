e69c3cdc88267e48587da09c52a128139f334d91
wy98 <1958134184@qq.com>
2018-08-24 11:29:51 +0800

    提交项目

---
+++ src/com/yr/Test/UserTest.java
@@ -0,0 +1,11 @@
+package com.yr.Test;
+
+public class UserTest {
+ public static void main(String[] args) {
+	UserTest.aa();
+	System.out.println(3333);
+}
+ public static void aa(){
+	 System.out.println(222);
+ }
+}
